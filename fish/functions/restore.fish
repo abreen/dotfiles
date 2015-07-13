06857ffb2ab6d4eba5ac801f85908dbba0bb55b0
Alexander Breen <alexander.breen@gmail.com>
2015-07-13 10:49:05 -0400

    Updated dotfiles

--- fish/functions/restore.fish
+++
@@ -1,3 +0,0 @@
-function restore --description 'Teleport to last saved directory'
-    cd (cat $HOME/.lastdir)
-end
--- fish/functions/save.fish
+++
@@ -1,3 +0,0 @@
-function save --description 'Save current directory for next time'
-    pwd > $HOME/.lastdir
-end
